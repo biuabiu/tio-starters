3483103cba38a5c92304196b994be75fbf198a33
fanpan26 <fanpan26@126.com>
2019-05-15 23:29:59 +0800

    Create TioWebSocketMsgHandlerNotFoundException.java

---
+++ tio-websocket-spring-boot-starter/src/main/java/com/fanpan26/tio/server/websocket/TioWebSocketMsgHandlerNotFoundException.java
@@ -0,0 +1,12 @@
+package com.fanpan26.tio.server.websocket;
+
+/**
+ * @author fyp
+ * @crate 2019/5/15 21:35
+ * @project tio-starters
+ */
+public class TioWebSocketMsgHandlerNotFoundException extends RuntimeException {
+    public TioWebSocketMsgHandlerNotFoundException() {
+        super();
+    }
+}
