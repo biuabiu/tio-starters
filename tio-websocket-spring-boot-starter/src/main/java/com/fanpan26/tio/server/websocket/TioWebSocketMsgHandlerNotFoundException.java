bab41ec37bc11e8abae9a738a719f7380455ffdc
Administrator <1532759525@qq.com>
2019-05-16 17:52:13 +0800

    test cluster

--- tio-websocket-spring-boot-starter/src/main/java/com/fanpan26/tio/server/websocket/TioWebSocketMsgHandlerNotFoundException.java
+++ tio-websocket-spring-boot-starter/src/main/java/com/fanpan26/tio/server/websocket/TioWebSocketMsgHandlerNotFoundException.java
@@ -2,8 +2,6 @@ package com.fanpan26.tio.server.websocket;
 
 /**
  * @author fyp
- * @crate 2019/5/15 21:35
- * @project tio-starters
  */
 public class TioWebSocketMsgHandlerNotFoundException extends RuntimeException {
     public TioWebSocketMsgHandlerNotFoundException() {
