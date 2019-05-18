253dae9832e2594b29a6691048b5c0942270af94
fanpan26 <fanpan26@126.com>
2019-05-18 23:15:29 +0800

    Create WebSocketAioListener.java

---
+++ tio-websocket-spring-boot-starter/src/main/java/com/fanpan26/tio/server/websocket/WebSocketAioListener.java
@@ -0,0 +1,12 @@
+package com.fanpan26.tio.server.websocket;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author fyp
+ * @crate 2019/5/18 23:14
+ * @project tio-starters
+ */
+@Service
+public @interface WebSocketAioListener {
+}
